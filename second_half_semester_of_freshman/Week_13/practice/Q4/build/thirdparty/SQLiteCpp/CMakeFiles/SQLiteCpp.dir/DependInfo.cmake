83b317ddd70faedd4a81cdb6903550eb19e27d04
IDK-Silver <60951976+IDK-Silver@users.noreply.github.com>
2023-06-20 18:17:18 +0800

    c++

---
+++ second_half_semester_of_freshman/Week_13/practice/Q4/build/thirdparty/SQLiteCpp/CMakeFiles/SQLiteCpp.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/idk/code/University/Week_13/practice/Q3/thirdparty/SQLiteCpp/src/Backup.cpp" "/home/idk/code/University/Week_13/practice/Q3/build/thirdparty/SQLiteCpp/CMakeFiles/SQLiteCpp.dir/src/Backup.cpp.o"
+  "/home/idk/code/University/Week_13/practice/Q3/thirdparty/SQLiteCpp/src/Column.cpp" "/home/idk/code/University/Week_13/practice/Q3/build/thirdparty/SQLiteCpp/CMakeFiles/SQLiteCpp.dir/src/Column.cpp.o"
+  "/home/idk/code/University/Week_13/practice/Q3/thirdparty/SQLiteCpp/src/Database.cpp" "/home/idk/code/University/Week_13/practice/Q3/build/thirdparty/SQLiteCpp/CMakeFiles/SQLiteCpp.dir/src/Database.cpp.o"
+  "/home/idk/code/University/Week_13/practice/Q3/thirdparty/SQLiteCpp/src/Exception.cpp" "/home/idk/code/University/Week_13/practice/Q3/build/thirdparty/SQLiteCpp/CMakeFiles/SQLiteCpp.dir/src/Exception.cpp.o"
+  "/home/idk/code/University/Week_13/practice/Q3/thirdparty/SQLiteCpp/src/Savepoint.cpp" "/home/idk/code/University/Week_13/practice/Q3/build/thirdparty/SQLiteCpp/CMakeFiles/SQLiteCpp.dir/src/Savepoint.cpp.o"
+  "/home/idk/code/University/Week_13/practice/Q3/thirdparty/SQLiteCpp/src/Statement.cpp" "/home/idk/code/University/Week_13/practice/Q3/build/thirdparty/SQLiteCpp/CMakeFiles/SQLiteCpp.dir/src/Statement.cpp.o"
+  "/home/idk/code/University/Week_13/practice/Q3/thirdparty/SQLiteCpp/src/Transaction.cpp" "/home/idk/code/University/Week_13/practice/Q3/build/thirdparty/SQLiteCpp/CMakeFiles/SQLiteCpp.dir/src/Transaction.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "SQLITE_ENABLE_COLUMN_METADATA"
+  "SQLITE_ENABLE_JSON1"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../thirdparty/SQLiteCpp/include"
+  "../thirdparty/SQLiteCpp/sqlite3"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/idk/code/University/Week_13/practice/Q3/build/thirdparty/SQLiteCpp/sqlite3/CMakeFiles/sqlite3.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
